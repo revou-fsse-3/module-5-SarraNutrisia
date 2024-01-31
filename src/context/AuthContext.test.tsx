1d341e6ae4263a5f33c5a1c87d162711d722e84c
Sarra Nutrisia <sarra.nutrisia@gmail.com>
2024-01-31 20:12:51 +0700

    Update Unit Testing Context

---
+++ src/context/AuthContext.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { AuthProvider } from '../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
+
+describe('AuthContext component unit testing', () => {
+    // this test for AuthContext component with explanation should render AuthContext with children
+    it('should render AuthContext with children', () => {
+        const ChildComponent = () => {
+            const { isAuthenticated, login, logout } = useAuth();
+            return (
+                <div>
+                    <p>{`isAuthenticated: ${isAuthenticated}`}</p>
+                    <button onClick={() => login('token')}>Login</button>
+                    <button onClick={() => logout()}>Logout</button>
+                </div>
+            );
+        };
+
+        render(
+            <AuthProvider>
+                <ChildComponent />
+            </AuthProvider>
+        );
+
+        const isAuthenticatedElement = screen.getByText(/isAuthenticated/i);
+        expect(isAuthenticatedElement).toMatchSnapshot();
+    });
+});
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+
+interface AuthContextProps {
+  isAuthenticated: boolean;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: React.ReactNode; // Tambahkan tipe untuk children
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Cek apakah token tersimpan di local storage
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  const login = (token: string) => {
+    localStorage.setItem('token', token);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
