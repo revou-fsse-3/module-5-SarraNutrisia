099d9be6f8a2ebbfaa12adcadb87c3855e15e75c
Sarra Nutrisia <sarra.nutrisia@gmail.com>
2024-01-31 18:13:03 +0700

    Adding Unit Testing Navbar

--- src/components/navbar/navbar.test.tsx
+++ src/components/navbar/navbar.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Navbar from '.';
 import { useRouter } from 'next/router';
 
@@ -18,4 +18,49 @@ describe('Navbar component unit testing', () => {
     const document = render(<Navbar />);
     expect(document).toMatchSnapshot();
   });
+  test('Clicking on the Home button navigates to /home', () => {
+    const mockPush = jest.fn();
+
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      pathname: '/',
+      push: mockPush,
+    }));
+
+    render(<Navbar />);
+    fireEvent.click(screen.getByRole('button', { name: /Home/i }));
+
+    expect(mockPush).toHaveBeenCalledWith('/home');
+  });
+
+  test('Clicking on the Register button navigates to /register', () => {
+    const mockPush = jest.fn();
+    
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      pathname: '/',
+      push: mockPush,
+    }));
+
+    render(<Navbar />);
+    
+    fireEvent.click(screen.getByRole('button', { name: /Register/i }));
+
+    expect(mockPush).toHaveBeenCalledWith('/register');
+  });
+
+  test('Clicking on the Register button navigates to /login', () => {
+    const mockPush = jest.fn();
+    
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      pathname: '/',
+      push: mockPush,
+    }));
+
+    render(<Navbar />);
+    
+    fireEvent.click(screen.getByRole('button', { name: /Login/i }));
+
+    expect(mockPush).toHaveBeenCalledWith('/login');
+  });
+
+
 });
